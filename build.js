6a489369a7e3c44740c0c1154a8de46105edca44
Lily Skye <me@suchipi.com>
2023-09-13 14:20:07 -0600

    build .tar.gz archives as part of build.js

--- build.js
+++ build.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env yavascript
+const version = require("./package.json").version;
 
-remove("dist");
+if (exists("dist")) {
+  remove("dist");
+}
+ensureDir("dist");
+
+const ysLicenseText = $([os.execPath(), "--license"]).stdout.trim();
+const diffLicenseText =
+  `-------------- diff (npm package) --------------` +
+  "\n\n" +
+  readFile("node_modules/diff/LICENSE");
+const hopicLicenseText =
+  `-------------- hopic --------------` + "\n\n" + readFile("LICENSE");
+
+writeFile(
+  "dist/LICENSE_ALL",
+  [ysLicenseText, diffLicenseText, hopicLicenseText].join("\n\n")
+);
+echo("./dist/LICENSE_ALL");
 
 exec(`npm run bundle`);
 
 const targetDirs = glob("node_modules/yavascript/bin/*");
-
 for (const targetDir of targetDirs) {
   const target = targetDir.segments.at(-1);
 
@@ -26,19 +43,20 @@ for (const targetDir of targetDirs) {
   chmod({ a: "rx", u: "rwx" }, outfile);
 
   echo(outfile.toString());
+
+  const tarGzPath = Path.resolve("dist");
+
+  copy("dist/LICENSE_ALL", Path.join(dirname(outfile), "LICENSE"));
+  const dirBefore = pwd();
+  try {
+    cd(dirname(outfile));
+    exec([
+      "tar",
+      "-czvf",
+      `${tarGzPath}/hopic-${version}-${target}.tar.gz`,
+      ".",
+    ]);
+  } finally {
+    cd(dirBefore);
+  }
 }
-
-const ysLicenseText = $([os.execPath(), "--license"]).stdout.trim();
-const diffLicenseText =
-  `-------------- diff (npm package) --------------` +
-  "\n\n" +
-  readFile("node_modules/diff/LICENSE");
-const hopicLicenseText =
-  `-------------- hopic --------------` + "\n\n" + readFile("LICENSE");
-
-writeFile(
-  "dist/LICENSE_ALL",
-  [ysLicenseText, diffLicenseText, hopicLicenseText].join("\n\n")
-);
-
-echo("./dist/LICENSE_ALL");
