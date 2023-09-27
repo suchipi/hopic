8ced8f76d2d5441e293baeea5c2a2819fb9e3c0d
Lily Skye <me@suchipi.com>
2023-09-26 23:36:17 -0600

    improve diff readability

--- src/diff.ts
+++ src/diff.ts
@@ -1,17 +1,29 @@
 import * as Diff from "diff";
 
 export function diff(a: string, b: string): string {
-  const diff = Diff.diffChars(a, b);
+  const diff = Diff.diffLines(a, b);
 
   return diff
     .map((part) => {
       // green for additions, red for deletions
       // grey for common parts
       const colorFn = part.added
-        ? (s: string) => bold(green(s))
+        ? (str: string) => bold(green("+ " + str))
         : part.removed
-        ? (s: string) => bold(red(s))
-        : grey;
+        ? (str: string) => bold(red("- " + str))
+        : (str: string) =>
+            grey(
+              str
+                .split("\n")
+                .map((line, index, all) => {
+                  if (index === all.length - 1 && line.trim().length === 0) {
+                    return line;
+                  } else {
+                    return "  " + line;
+                  }
+                })
+                .join("\n")
+            );
 
       return colorFn(part.value);
     })
